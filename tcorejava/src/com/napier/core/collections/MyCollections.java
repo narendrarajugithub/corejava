8fd2912da34e40bd4df3f16709d7f035957f16ae
narendrarajugithub <ynarendraraju@gmail.com>
2018-04-09 22:49:38 +0530

    Update MyCollections.java

--- tcorejava/src/com/napier/core/collections/MyCollections.java
+++ tcorejava/src/com/napier/core/collections/MyCollections.java
@@ -15,6 +15,7 @@ public class MyCollections {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//THIS LINE IS ADDED BY SIVA
+		//This line is added from url
 		ArrayList<Integer> a=new ArrayList<Integer>();
 		a.add(1);
 		a.add(5);
