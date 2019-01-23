7aa94c399bf863294be449af4af494d8a7da0488
narendraraju <narendraraju55@gmail.com>
2019-01-23 14:31:36 +0530

    test

---
+++ tcorejava/src/com/napier/core/BasicProgram.java
@@ -0,0 +1,14 @@
+package com.napier.core;
+
+public class BasicProgram {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println("c=="+c);
+	}
+
+}
